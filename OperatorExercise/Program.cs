79b859dc658c5822d3c045829cf8b9706caee1b3
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-20 15:11:12 -0400

    EX1 fix

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            Console.WriteLine(ExerciseOne);
+            ExerciseOne();
         }
 
         public static void ExerciseOne()
@@ -16,7 +16,7 @@
             int remainder = a % b;
             if (a == 17 && b == 4)
             {
-                 Console.WriteLine($"{quotient} is {b} {remainder} 1");
+                 Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
             }
         }
         
