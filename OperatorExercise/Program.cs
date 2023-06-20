3de5b78525fbfe2b9bdf6b3fb3e1c9ebb941f9dd
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-20 15:00:34 -0400

    EX1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine(ExerciseOne);
         }
+
+        public static void ExerciseOne()
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            {
+                 Console.WriteLine($"{quotient} is {b} {remainder} 1");
+            }
+        }
+        
     }
 }
